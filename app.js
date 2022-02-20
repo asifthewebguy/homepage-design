f5e19068bf73c17d44840ebec694dda91d08399b
Mohammad Asif Ullah Chowdhury <78401115+asifthewebguy@users.noreply.github.com>
2022-02-21 01:03:06 +0600

    added livereload connect-live reload & configured

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const { log } = require('console');
+const log = require('console');
 const express = require('express');
+const livereload = require('livereload');
+const connectLivereload = require('connect-livereload');
 const app = express();
 const port = 8080;
+
+const liveReloadServer = livereload.createServer();
+app.use(connectLivereload({
+    port: 35729
+}));
 app.use(express.static('dist'));
 app.use(express.static('src'));
 app.use(express.static('assets'));
 
+liveReloadServer.watch(__dirname + '/dist');
+liveReloadServer.watch(__dirname + '/src');
+// liveReloadServer.server.once("connection", () => {
+//     setTimeout(() => {
+//         liveReloadServer.refresh("/");
+//     }, 100);
+// });
+
+
 app.get('/', (req, res) => {
     res.send('index.html');
-
 });
 
 app.listen(port, () => {
